b0d3b4158a04175694a7f5a2f251fc5829683451
Diogo Amador <129464637+diogomador@users.noreply.github.com>
2023-12-06 20:06:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,10 +11,35 @@ function login() {
 function register() {
     var usuario = document.getElementById("usuario").value;
     var senha = document.getElementById("senha").value;
-    var option = document.getElementById("choice").value
+    var escolha = document.getElementById("choice").value
 
     window.location.href = "index.html";
 
     document.getElementById("usuario").value = "";
     document.getElementById("senha").value = "";
+
+    if (escolha === "CL") {
+        console.log("Cliente selecionado");
+
+    }
+
+    else if (escolha === "VE") {
+        console.log("Vendedor selecionado");
+    }
+
+    else {
+        console.log("Escolha inválida");
+    }
+}
+
+function mostrarSenha() {
+    var senha = document.getElementById("senha");
+
+    if (senha.type === "password") {
+        senha.type = "text";
+    } 
+
+    else {
+        senha.type = "password";
+    }
 }
