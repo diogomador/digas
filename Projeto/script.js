23b238c37b85bc224d8d121dd71a71329d39e014
Diogo Amador <129464637+diogomador@users.noreply.github.com>
2023-12-20 16:06:11 -0300

    Add files via upload

---
+++ Projeto/script.js
@@ -0,0 +1,12 @@
+function criarConta(){
+    window.location.href = "register.html";
+}
+
+function logar(){
+    window.location.href = "login.html";
+}
+
+function carrinho(){
+    window.location.href = "carrinho.html";
+}
+
